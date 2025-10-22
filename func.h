f1624f2e799d6612e49f1d321bdd9ead84c28ae1
ArthurFerrariUlbrich <afulbrich@gmail.com>
2025-10-22 16:09:10 -0300

    .h

--- func.h
+++ func.h
@@ -3,16 +3,16 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include "tarefa.h"
 
 using namespace std;
 
+std::string get_color_code(const std::string& cor);
+void print_gantt(const std::vector<Tarefa>& tarefas, const std::vector<int>& running_task, int current_time);
 void fifo(vector<Tarefa>& tarefas);
 void srtf(vector<Tarefa>& tarefas);
 void priop(vector<Tarefa>& tarefas);
-
 std::vector<Tarefa> carregarConfiguracao();
 
 #endif
-
-
